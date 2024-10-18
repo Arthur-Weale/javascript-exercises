705b2c604a4fb723efb9beb20b826eabae84f8a1
Arthur Weale <ajayrossy340@gmail.com>
2024-10-18 21:01:04 +0200

    Created loop, yet to verify if the code works :)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
 
+
+stringToBeReturned = ""
+
+const repeatString = function(_greeting, numOfTimes) {
+    for( let greeting = 0; greeting < numOfTimes; greeting++){
+    return stringToBeReturned;
+    }
 };
+
+repeatString('hey', 3) // returns 'heyheyhey'
+
 
 // Do not edit below this line
 module.exports = repeatString;
