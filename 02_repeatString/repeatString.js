bf48e4f060b7d2b55f060674243c64f3736faf29
Arthur Weale <ajayrossy340@gmail.com>
2024-10-28 16:53:50 +0200

    This exercise is complete.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,8 +3,12 @@
 
 
 const repeatString = function(greeting, numOfTimes) {
+
+    if (numOfTimes < 0){
+        return 'ERROR'
+    }
+
     stringToBeReturned = ""
-    number = ""
     for( let i = 0; i < numOfTimes; i++){
         
         stringToBeReturned += greeting;
@@ -13,16 +17,12 @@ const repeatString = function(greeting, numOfTimes) {
 };
 
 repeatString('hey', 3) // returns 'heyheyhey'
-repeatString('hello', 10) // returns 'heyheyhey'
-repeatString('hi', 1) // returns 'heyheyhey'
-repeatString('bye', 0) // returns 'heyheyhey'
-repeatString('goodbye', -1) // returns 'heyheyhey'
-repeatString('', 10) // returns 'heyheyhey'
-repeatString('odin', number) // returns 'heyheyhey'
-// repeatString('hey', 3) // returns 'heyheyhey'
-// repeatString('hey', 3) // returns 'heyheyhey'
-// repeatString('hey', 3) // returns 'heyheyhey'
-// repeatString('hey', 3) // returns 'heyheyhey'
+repeatString('hello', 10) // returns 'hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello'
+repeatString('hi', 1) // returns 'hi'
+repeatString('bye', 0) // returns ''
+repeatString('goodbye', -1) // returns 'ERROR'
+repeatString('', 10) // returns ''
+repeatString('odin') // returns 'odin*999'
 
 
 
