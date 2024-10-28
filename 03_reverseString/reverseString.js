ae39d1d497b0e0319a5961a82d7c6e3cf1939556
Arthur Weale <ajayrossy340@gmail.com>
2024-10-28 17:11:18 +0200

    Completed and passed the first test.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    splitString = string.split("").reverse().join("");
+    return splitString;
 };
 
+reverseString('hello')
+//reverseString('hello there')
 // Do not edit below this line
 module.exports = reverseString;
