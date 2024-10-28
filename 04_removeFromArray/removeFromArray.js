feb3fd0d78b107e91b271bc33e41a9739f047f85
Arthur Weale <ajayrossy340@gmail.com>
2024-10-28 20:46:57 +0200

    Completed removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, removed){
+    lengthOfArray = arr.length;
+    const newArray = []
+    for(let i = 1; i <= lengthOfArray; i++){
+        if (i != removed){
+            newArray.push(i)
+        }
+    }
+    return newArray
 };
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
