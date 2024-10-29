d45a977c4cffff5a1b4b90bd3502592078563652
Arthur Weale <ajayrossy340@gmail.com>
2024-10-30 00:03:00 +0200

    13 out of 16 test completed. 3 more to go.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
-const removeFromArray = function(arr, removed, removed2){
+const removeFromArray = function(arr, removed, removed2, removed3, removed4){
     lengthOfArray = arr.length;
     const newArray = []
     for(let i = 1; i <= lengthOfArray; i++){
-        if (i != removed  && i != removed2){
+        if (i != removed  && i != removed2 && i != removed3  && i != removed4){
             newArray.push(i)
         }
     }
@@ -11,12 +11,12 @@ const removeFromArray = function(arr, removed, removed2){
 
 removeFromArray([1, 2, 3, 4], 3);
 removeFromArray([1, 2, 3, 4], 3, 2);
-// removeFromArray([1, 2, 3, 4], 3);
-// removeFromArray([1, 2, 3, 4], 3);
-// removeFromArray([1, 2, 3, 4], 3);
-// removeFromArray([1, 2, 3, 4], 3);
-// removeFromArray([1, 2, 3, 4], 3);
-// removeFromArray([1, 2, 3, 4], 3);
+//removeFromArray([1, 2, 2, 3], 2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 7, 2);
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"],"hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 // removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
