1a3381403d8162a13a30e78631972f4217d86084
Arthur Weale <ajayrossy340@gmail.com>
2024-10-29 23:28:43 +0200

    Completed the second remove from array exercise. Very much proud.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
-const removeFromArray = function(arr, removed){
+const removeFromArray = function(arr, removed, removed2){
     lengthOfArray = arr.length;
     const newArray = []
     for(let i = 1; i <= lengthOfArray; i++){
-        if (i != removed){
+        if (i != removed  && i != removed2){
             newArray.push(i)
         }
     }
@@ -10,5 +10,13 @@ const removeFromArray = function(arr, removed){
 };
 
 removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+// removeFromArray([1, 2, 3, 4], 3);
+// removeFromArray([1, 2, 3, 4], 3);
+// removeFromArray([1, 2, 3, 4], 3);
+// removeFromArray([1, 2, 3, 4], 3);
+// removeFromArray([1, 2, 3, 4], 3);
+// removeFromArray([1, 2, 3, 4], 3);
+// removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
