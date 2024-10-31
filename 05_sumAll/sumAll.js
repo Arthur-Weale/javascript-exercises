a16542d6a3974470ffbe99c40f4909d2e97d55a2
Arthur Weale <ajayrossy340@gmail.com>
2024-10-31 18:11:52 +0200

    Might come back to this, will try a different approach

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
 
+
+const sumAll = function(start, end) {
+    let finalSum = 0;
+    if(Number.isInteger(start) === false || Number.isNaN(end) === false || start < 1 ){
+        return 'ERROR';
+    }else if(start <= 1){ 
+        for(let i = start; i <= end; i++ ){
+            finalSum += i;
+        }
+    }else if(start > 1){
+        for(let i = end; i <= start; i++ ){
+            finalSum += i;
+        }
+    }
+    return finalSum;
 };
 
+sumAll(1, 4)
+sumAll(1, 4000)
+sumAll(123, 1)
+sumAll(-10, 4)
+sumAll(2.5, 4)
+sumAll(10, "90")
+sumAll(10, [90, 1])
 // Do not edit below this line
 module.exports = sumAll;
