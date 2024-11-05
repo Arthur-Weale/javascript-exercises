8feb890b0ee5e61232ef997e15d263aba6adc871
Arthur Weale <ajayrossy340@gmail.com>
2024-11-05 16:23:10 +0200

    Completed all convert to Celsius tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  tempValue = ((temperatureInFahrenheit - 32) * 5/9).toFixed(1);
+  return parseFloat(tempValue);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  tempValue = ((temperatureInCelsius * 9/5) + 32).toFixed(1);
+  return parseFloat(tempValue)
 };
 
 // Do not edit below this line
