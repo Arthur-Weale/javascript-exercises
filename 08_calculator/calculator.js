a216a4d525c15d3761bb055de181952155eef348
Arthur Weale <ajayrossy340@gmail.com>
2024-11-06 19:54:31 +0200

    33 tests pass out of 38

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(value1,value2) {
+  return value1 + value2;
 };
 
-const subtract = function() {
-	
+const subtract = function(value1,value2) {
+	return value1 - value2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let runningTotal = 0;
+	for(value of arr){
+    runningTotal = runningTotal + value;
+  }
+  return runningTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let runningMultiple = 1
+  for(value of arr){
+    runningMultiple = runningMultiple * value;
+  };
+  return runningMultiple;
 };
 
-const power = function() {
-	
+const power = function(value1,value2) {
+	return value1 ** value2;
 };
 
-const factorial = function() {
-	
+const factorial = function(value1) {
+  let newValue = 1
+	arr = [];
+  arr.push(value1)
+  for(value in _Range(arr)){
+    newValue = newValue * value;
+  }
+  return newValue;
 };
 
 // Do not edit below this line
