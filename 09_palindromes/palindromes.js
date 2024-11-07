ac3bda162acf48b4340e4d87313be830f4b80650
Arthur Weale <ajayrossy340@gmail.com>
2024-11-07 16:44:19 +0200

    Palindromes test success and complete.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string){
+    let punc = /[.!, ]/g
+    const oldString = string.trim().replace(punc,'');
+    const newString = string.trim().replace(punc,'').split("").reverse().join("");
+        console.log(oldString);
+        if(oldString.toLowerCase() === newString.toLowerCase()){
+            return true;
+        }else 
+            return false;
 };
 
+console.log(palindromes('A car, a man, a maraca.'));
 // Do not edit below this line
 module.exports = palindromes;
