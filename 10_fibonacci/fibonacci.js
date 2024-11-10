2cbd7267098405738f85c7d8915092e95a0afbd7
Arthur Weale <ajayrossy340@gmail.com>
2024-11-11 01:37:06 +0200

    Completed the loop system.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
 
+let firstPrecedingValue = 0;
+let secondPrecedingValue = 1;
+let newPrecedingValue = 0;
+const fibonacciArray = [];
+
+const fibonacci = function(fibonacciValue) {
+    fibonacciArray.push(secondPrecedingValue);
+    
+    for(i = 1; i < fibonacciValue; i++){
+
+        newPrecedingValue = firstPrecedingValue + secondPrecedingValue;
+        firstPrecedingValue = secondPrecedingValue;
+        secondPrecedingValue = newPrecedingValue;
+        fibonacciArray.push(newPrecedingValue);
+    }
+    console.log(fibonacciArray);
+    let fibonacciArrayLength = fibonacciArray.length;
+
+    if(fibonacciArrayLength == fibonacciValue){
+        let actualfibonacciValue = fibonacciArray[fibonacciValue]; 
+    }
+    //return actualfibonacciValue;
 };
 
+console.log(fibonacci(4))
 // Do not edit below this line
-module.exports = fibonacci;
+//module.exports = fibonacci;
