dae5c88c3e81aff93f722842c975f7a929766ab5
Arthur Weale <ajayrossy340@gmail.com>
2024-11-11 23:20:22 +0200

    Removed Dead code and added comments.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,10 +2,5 @@ const reverseString = function(string) {
     splitString = string.split("").reverse().join("");
     return splitString;
 };
-
-reverseString('hello')
-reverseString('hello there')
-reverseString('123! abc! Hello, Odinite')
-reverseString('')
 // Do not edit below this line
 module.exports = reverseString;
