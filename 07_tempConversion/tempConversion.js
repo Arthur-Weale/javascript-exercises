04c2990881d5046718d3884ce33ad6d95e3602f1
Arthur Weale <ajayrossy340@gmail.com>
2024-11-11 23:34:40 +0200

    Added comments

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,12 @@
 const convertToCelsius = function(temperatureInFahrenheit) {
-  tempValue = ((temperatureInFahrenheit - 32) * 5/9).toFixed(1);
+  tempValue = ((temperatureInFahrenheit - 32) * 5/9).toFixed(1);//Contains fahrenheit to celsius formula and calculates to 1 significant value using the toFixed(1).
   return parseFloat(tempValue);
 };
 
 const convertToFahrenheit = function(temperatureInCelsius) {
-  tempValue = ((temperatureInCelsius * 9/5) + 32).toFixed(1);
+  tempValue = ((temperatureInCelsius * 9/5) + 32).toFixed(1);//Contains celsius to fahrenheit formula and calculates.
   return parseFloat(tempValue)
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
