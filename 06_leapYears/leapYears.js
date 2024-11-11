c622697e6e75f9c0694d35a03fa29d2f3b3cff26
Arthur Weale <ajayrossy340@gmail.com>
2024-11-11 23:31:01 +0200

    Added comments and removed dead code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,10 @@
 const leapYears = function(year) {
-
+    //Checks if year is divisible and leaves no remainders.
     if(year % 4 == 0 && year % 100 != 0|| year % 4 == 0 &&  year % 100  == 0 &&  year % 400 == 0 ){
-        return true;
+        return true;//Returns true 
     }else if(year % 4 != 0 || year % 4 == 0 && year % 100 == 0){
         return false;
     }
 };
-
 // Do not edit below this line
 module.exports = leapYears;
