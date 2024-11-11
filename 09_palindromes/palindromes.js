3311006075e939d2cdbc1d51c15e11b2b5930a2a
Arthur Weale <ajayrossy340@gmail.com>
2024-11-11 23:51:42 +0200

    Removed dead code and added comments.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,12 @@
 const palindromes = function (string){
-    let punc = /[.!, ]/g
-    const oldString = string.trim().replace(punc,'');
-    const newString = string.trim().replace(punc,'').split("").reverse().join("");
-        console.log(oldString);
+    let punc = /[.!, ]/g;//variable contains punctuation marks to be ignore.
+    const oldString = string.trim().replace(punc,'');//Removes white spaces using the trim built in function and uses replace to replace the punctuction marks with an empty space.
+    const newString = string.trim().replace(punc,'').split("").reverse().join("");//Does much of whats being done above and splits the string into an array of elements and reverses and joins together at the end.
+        //Using strict operator to compare if both cases are equal.
         if(oldString.toLowerCase() === newString.toLowerCase()){
             return true;
         }else 
             return false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
