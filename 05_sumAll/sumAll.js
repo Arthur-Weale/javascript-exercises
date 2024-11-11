6b2baa362c6aa47dbe6256d6d56505a0b3c3209b
Arthur Weale <ajayrossy340@gmail.com>
2024-11-11 23:28:29 +0200

    Removed dead code and added comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,14 @@
-
-
+//Sum all function.
 const sumAll = function(start, end) {
+    //Re-initialising the final sum variable to zero.
     let finalSum = 0;
+    //checks if start is an integer or if less than 1 and retuns "ERROR" if one of the option is true.
     if(Number.isInteger(start) === false || Number.isInteger(end) === false || start < 1 ){
         return 'ERROR';
-
+    //If start is less than or equal to 1 then logic add 1 to finalSum.
     }else if(start <= 1){ 
         for(let i = start; i <= end; i++ ){
-            finalSum += i;
+            finalSum += i;//Increments
         }
     }
     else if(start > 1){
@@ -15,16 +16,7 @@ const sumAll = function(start, end) {
             finalSum += i;
         }
     }
-    return finalSum;
-    //console.log(finalSum);
+    return finalSum; //Returns final sum
 };
-
-sumAll(1, 4)
-sumAll(1, 4000)
-sumAll(123, 1)
-sumAll(-10, 4)
-sumAll(2.5, 4)
-sumAll(10, "90")
-sumAll(10, [90, 1])
 // Do not edit below this line
 module.exports = sumAll;
