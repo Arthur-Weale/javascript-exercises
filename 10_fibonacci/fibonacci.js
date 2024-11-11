48b64eaa4d9ee1a477e9512a1abbe65d10eef0ab
Arthur Weale <ajayrossy340@gmail.com>
2024-11-11 23:00:28 +0200

    Tests complete and a success

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -2,27 +2,23 @@
 let firstPrecedingValue = 0;
 let secondPrecedingValue = 1;
 let newPrecedingValue = 0;
-const fibonacciArray = [];
+const fibonacciArray = []; //Re-initiallizes a new array.
 
 const fibonacci = function(fibonacciValue) {
-    fibonacciArray.push(secondPrecedingValue);
-    
-    for(i = 1; i < fibonacciValue; i++){
+    fibonacciArray.push(firstPrecedingValue); // adds the first digit 0 in the array.
+    fibonacciArray.push(secondPrecedingValue); // add the second digit 1 in the array.
 
-        newPrecedingValue = firstPrecedingValue + secondPrecedingValue;
-        firstPrecedingValue = secondPrecedingValue;
-        secondPrecedingValue = newPrecedingValue;
+//Pre-Check if the fibonacci value is less than 0. 
+    if(fibonacciValue < 0)return "OOPS";
+// For loop to iterate and make values to be generated.
+    for(i = 0; i < 26; i++){
+        newPrecedingValue = firstPrecedingValue + secondPrecedingValue; // logic to add the two preceding values.
+        firstPrecedingValue = secondPrecedingValue; //This creates a shift to make the second value the first value.
+        secondPrecedingValue = newPrecedingValue; //This creates a shift to make the new value calculated to be the second value.
         fibonacciArray.push(newPrecedingValue);
     }
-    console.log(fibonacciArray);
-    let fibonacciArrayLength = fibonacciArray.length;
-
-    if(fibonacciArrayLength == fibonacciValue){
-        let actualfibonacciValue = fibonacciArray[fibonacciValue]; 
-    }
-    //return actualfibonacciValue;
+    let actualFibonacciValue = fibonacciArray[fibonacciValue];
+    return actualFibonacciValue;
 };
-
-console.log(fibonacci(4))
 // Do not edit below this line
-//module.exports = fibonacci;
+module.exports = fibonacci;
