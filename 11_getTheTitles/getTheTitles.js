af06b2e0afd9f16079ec97c75c04e46c1080743f
Arthur Weale <ajayrossy340@gmail.com>
2024-11-12 14:35:22 +0200

    Testing using console.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+
+
+const books = [
+    {
+    title: 'Book',
+    author: 'Name'
+    },
+    {
+    title: 'Book2',
+    author: 'Name2'
+    }
+]
+
+const getTheTitles = function(books) {
+    for(aTitle of books){
+        bookTitle = aTitle.filter(title);
+        console.log(bookTitle);
+        //return bookTitle;
+    }
 
 };
-
+getTheTitles(books)
 // Do not edit below this line
-module.exports = getTheTitles;
+//module.exports = getTheTitles;
