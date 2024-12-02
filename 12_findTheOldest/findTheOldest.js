9f587f7eb875e5462144d43681152bd36e964767
Arthur Weale <ajayrossy340@gmail.com>
2024-12-02 15:08:40 +0200

    Second test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,15 +1,14 @@
 const people = [
     {
     name: "Carly",
-    yearOfBirth: 1942,
-    yearOfDeath: 1970,
+    yearOfBirth: 2018,
     },
     {
     name: "Ray",
     yearOfBirth: 1962,
     yearOfDeath: 2011,
     },
-{
+    {
     name: "Jane",
     yearOfBirth: 1912,
     yearOfDeath: 1941,
@@ -39,6 +38,7 @@ const findTheOldest = function(people) {
 }
     console.log(`The oldest is : ${recentOldestPerson}`)
     console.log(oldestPersonObject);
+    //return oldestPersonObject;
     }
 findTheOldest(people);
 console.log();
