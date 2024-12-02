f189d414b6e819c07bcf592c01080aea575b7b93
Arthur Weale <ajayrossy340@gmail.com>
2024-12-02 17:23:13 +0200

    All tests complete.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,23 +1,24 @@
-const people = [
-    {
-    name: "Carly",
-    yearOfBirth: 2018,
-    },
-    {
-    name: "Ray",
-    yearOfBirth: 1962,
-    yearOfDeath: 2011,
-    },
-    {
-    name: "Jane",
-    yearOfBirth: 1912,
-    yearOfDeath: 1941,
-    },
-]
+// const people = [
+//     {
+//     name: "Carly",
+//     yearOfBirth: 2018,
+//     },
+//     {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//     },
+//     {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//     },
+// ]
 
 let oldestPersonObject = null
 let nameOfPerson = ''
 let recentOldestPerson = 0
+let  currentDate = new Date();
 
 const findTheOldest = function(people) {
 
@@ -26,21 +27,26 @@ const findTheOldest = function(people) {
         dateOfBirth = person.yearOfBirth;
         dateOfDeath = person.yearOfDeath;
 
+        if( dateOfDeath === undefined ){
+            dateOfDeath = currentDate.getFullYear();
+        }
+            //console.log(dateOfDeath);
+
         let currentOldestPerson = dateOfDeath - dateOfBirth;
-            console.log(nameOfPerson);
-            console.log(recentOldestPerson);
-            console.log(currentOldestPerson);
+            // console.log(nameOfPerson);
+            // console.log(recentOldestPerson);
+            // console.log(currentOldestPerson);
 
         if(currentOldestPerson > recentOldestPerson){
             recentOldestPerson = currentOldestPerson;
             oldestPersonObject = person;
     }
 }
-    console.log(`The oldest is : ${recentOldestPerson}`)
-    console.log(oldestPersonObject);
-    //return oldestPersonObject;
+    // console.log(`The oldest is : ${recentOldestPerson}`)
+    // console.log(oldestPersonObject);
+    return oldestPersonObject;
     }
-findTheOldest(people);
-console.log();
+//findTheOldest(people);
+//console.log();
 // Do not edit below this line
-//module.exports = findTheOldest;
+module.exports = findTheOldest;
